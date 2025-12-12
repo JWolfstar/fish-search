989cbf83de47dd9b928a7256f57b1670f054b29a
Paxton Stokes <paxstokes@gmail.com>
2025-12-12 16:50:25 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string[] fishArray = {"goldfish", "pufferfish", "clownfish", "bass", "flounder", "salmon"};
+
+Console.Write("Fish search: ");
+string searchTerm = Console.ReadLine();
+Console.WriteLine( SearchForFish(searchTerm) );
+
+string SearchForFish(string searchTerm)
+{
+    foreach (string fish in fishArray)
+    {
+        string lowerFish = fish.ToLower();
+        string lowerSearchTerm = searchTerm.ToLower();
+
+        if (lowerFish.Contains(lowerSearchTerm))
+        {
+            return $"Found fish: {fish}";
+        }
+    }
+
+    return $"Did not find fish matching \"{searchTerm}\"";
+}
